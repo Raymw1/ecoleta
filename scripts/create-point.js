45df81d8c2a84b7ba23a833664b03b69a7bfa539
Rayan Wilbert <rayan.rmw@gmail.com>
2021-06-02 15:42:34 -0300

    Get city name, beggining the items list

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -2,7 +2,7 @@
 // -------------------------  STATES ---------------------------
 let ufField = document.querySelector("select[name=uf]");
 let stateInput = document.querySelector("input[name=state]");
-let citiesField = document.querySelector("select[name=city]");
+let citiesField = document.querySelector("select[name=cities]");
 function populateUFs() {
     let ufURL = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
     getAPI(ufURL, ufField);
@@ -13,15 +13,20 @@ function getCities(event) {
     stateInput.value = event.target.options[indexSelectedState].text;
     let ufValue = event.target.value;
     let cityURL = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
-    citiesField.disabled = false;
-    citiesField.innerHTML = "";
+    citiesField.innerHTML = `<option value="" disabled selected>Selecione a cidade</option>`;
+    citiesField.disabled = true;
     getAPI(cityURL, citiesField);
+    citiesField.disabled = false;
 }
 
 function getAPI(url, field) {
     fetch(url).then( res => res.json() ).then( values => {
         for (value of values) {
-            field.innerHTML += `<option value="${value.id}">${value.nome}</option>`;
+            if (field == citiesField) {
+                field.innerHTML += `<option value="${value.nome}">${value.nome}</option>`;
+            }   else {
+                field.innerHTML += `<option value="${value.id}">${value.nome}</option>`;
+            }
         }
     })
 }
